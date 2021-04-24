1759b1acad0fecfdb5e2f1e521686c832b045939
mamatap13 <mamatapolisetty@gmail.com>
2021-04-24 13:40:06 -0700

    profile menu, not connected, login to see this view

---
+++ app/src/main/java/com/example/vaxdiscussions/ProfileMenu.java
@@ -0,0 +1,14 @@
+package com.example.vaxdiscussions;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_menu);
+    }
+}
