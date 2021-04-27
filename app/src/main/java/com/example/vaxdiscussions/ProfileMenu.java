95464493e5f5e68475e8e59373683dad70331406
mamatap13 <mamatapolisetty@gmail.com>
2021-04-27 10:23:17 -0700

    settings page

--- app/src/main/java/com/example/vaxdiscussions/ProfileMenu.java
+++ app/src/main/java/com/example/vaxdiscussions/ProfileMenu.java
@@ -2,7 +2,10 @@ package com.example.vaxdiscussions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 
 public class ProfileMenu extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class ProfileMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_menu);
+    }
+
+    public void launchSettingsMenu(View view) {
+        Intent intent = new Intent(this, SettingsMenu.class);
+        startActivity(intent);
     }
 }
