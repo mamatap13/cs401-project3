566067554cb2ca3b3d086c980c9a1fd21f31f20f
Joshua Deanon <jdeanon@horizon.csueastbay.edu>
2021-05-04 07:12:22 -0700

    Logout button added

--- app/src/main/java/com/example/vaxdiscussions/activities/SettingsMenu.java
+++ app/src/main/java/com/example/vaxdiscussions/activities/SettingsMenu.java
@@ -2,9 +2,13 @@ package com.example.vaxdiscussions.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.vaxdiscussions.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 /**
@@ -16,9 +20,22 @@ public class SettingsMenu extends AppCompatActivity {
      * Settings menu oncreate for notification and logout
      * @param savedInstanceState
      */
+
+    private Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_menu);
+
+        logout = (Button) findViewById(R.id.button_logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(SettingsMenu.this, MainActivity.class));
+            }
+        });
     }
 }
