790323bdadad6a9a4da69caac505a2ffdf011f71
mamatap13 <mamatapolisetty@gmail.com>
2021-05-03 21:15:11 -0700

    Reply_page

--- app/src/main/java/com/example/vaxdiscussions/Reply_page.java
+++ app/src/main/java/com/example/vaxdiscussions/Reply_page.java
@@ -2,13 +2,27 @@ package com.example.vaxdiscussions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Reply_page extends AppCompatActivity {
 
+    Button enter_reply;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reply_page);
+
+        enter_reply = findViewById(R.id.button_enter_Reply);
+
+        enter_reply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Reply_page.this, CovidDiscussions.class);
+                startActivity(intent);
+            }
+        });
     }
 }
