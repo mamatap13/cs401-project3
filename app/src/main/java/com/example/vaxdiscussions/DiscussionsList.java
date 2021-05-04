34ed643bb0effec00e8e8e70f62c4d904fc52fc6
sitara <smeherzad481@gmail.com>
2021-05-03 19:49:19 -0700

    Created login- activity and connected the discussion list.

--- app/src/main/java/com/example/vaxdiscussions/DiscussionsList.java
+++ app/src/main/java/com/example/vaxdiscussions/DiscussionsList.java
@@ -9,19 +9,15 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class DiscussionsList extends AppCompatActivity {
-    EditText covid_19, childhood_vaccine,why_vaccine,side_effect,moderna_or_pfizer;
+
     Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.discussions_list);
 
-        covid_19 = findViewById(R.id.disc_covid);
-        childhood_vaccine = findViewById(R.id.childhood_vaccines);
-        why_vaccine = findViewById(R.id.topic_vaccine);
-        side_effect = findViewById(R.id.side_effect_stories);
-        moderna_or_pfizer = findViewById(R.id.mode_or_pfiz);
-        login = findViewById(R.id.button_main_login);
+
+
 
         login.setOnClickListener(v -> {
             Intent intent = new Intent(DiscussionsList.this, LoginMenu.class);
--- app/src/main/java/com/example/vaxdiscussions/Discussions_after_login.java
+++ app/src/main/java/com/example/vaxdiscussions/Discussions_after_login.java
@@ -2,13 +2,61 @@ package com.example.vaxdiscussions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Discussions_after_login extends AppCompatActivity {
+    EditText covid_19, childhood_vaccine,why_vaccine,side_effect,moderna_or_pfizer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discussions_after_login);
+
+        covid_19 = findViewById(R.id.disc_covid);
+        childhood_vaccine = findViewById(R.id.childhood_vaccines);
+        why_vaccine = findViewById(R.id.topic_vaccine);
+        side_effect = findViewById(R.id.side_effect_stories);
+        moderna_or_pfizer = findViewById(R.id.mode_or_pfiz);
+
+        covid_19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Discussions_after_login.this, LoginMenu.class); //change to covid_19mpage
+                startActivity(intent);
+            }
+        });
+        childhood_vaccine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Discussions_after_login.this, LoginMenu.class); //change to covid_19mpage
+                startActivity(intent);
+            }
+        });
+        why_vaccine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Discussions_after_login.this, LoginMenu.class); //change to covid_19mpage
+                startActivity(intent);
+            }
+        });
+        side_effect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Discussions_after_login.this, LoginMenu.class); //change to covid_19mpage
+                startActivity(intent);
+            }
+        });
+        moderna_or_pfizer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Discussions_after_login.this, LoginMenu.class); //change to covid_19mpage
+                startActivity(intent);
+            }
+        });
+
     }
 }
